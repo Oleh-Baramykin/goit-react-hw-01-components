a1ab1d829015130e320fe267fc5a422529bbc483
Oleh Baramykin <99210252+Oleh-Baramykin@users.noreply.github.com>
2023-02-01 12:21:04 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -42,9 +42,15 @@ export const Profile = ({ avatar, username, tag, location, stats }) => {
 };
 
 Profile.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  stats: PropTypes.object.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }),
+  }),
 };
